393e5b76f909667d89cc4e7f2c78b7ce39b6d9ec
Helder Mendes <helder.nmendes@gmail.com>
2025-01-29 14:25:27 -0300

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,46 @@
+// Menu Sidebar
+
+
+const menu__sidebar = document.querySelector('#menu__sidebar');
+const menu__open_btn = document.querySelector('#menu__open-btn');
+menu__open_btn.addEventListener('click', function(){
+    menu__sidebar.style.display = 'block';
+
+    const menu__close_btn = document.querySelector('#menu__close-btn');
+    menu__close_btn.addEventListener('click', function(){
+        menu__sidebar.style.display = 'none';
+    })
+
+    if (document.querySelector('body').style.width > '1600px'){
+        menu__sidebar.style.display = 'block';  
+    }
+})
+
+// Menu btns
+
+const code_editor__btn = menu__sidebar.querySelector('#code-editor__btn');
+const community__btn = menu__sidebar.querySelector('#community__btn');
+
+const code_editor = document.querySelector('#code-editor');
+const code_editor__custom = document.querySelector('#code-editor__custom');
+const community = document.querySelector('#community')
+
+code_editor__btn.addEventListener('click', function(){
+    code_editor.style.display = 'block';
+    code_editor__custom.style.display = 'block';
+    code_editor__btn.classList.remove = 'unselect';
+
+    community.style.display = 'none';
+    community__btn.classList.add = 'unselect';
+})
+
+community__btn.addEventListener('click', function(){
+    code_editor.style.display = 'none';
+    code_editor__custom.style.display = 'none';
+    code_editor__btn.classList.add = 'unselect';
+
+    community.style.display = 'block';
+    community__btn.classList.remove = 'unselect';
+})
+
+
